a90dbca220e40946cc225eade1b3f55206145f7f
jmoypace <jmoypace@gmail.com>
2018-06-27 14:56:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 # Add  code here!
 def prime?(num)
 
-
-  Math.sqrt(num).floor.downto(2).each {|i| return false if num % i == 0}
-   true
-
+  if num < 0 or num == 0 or num == 1
+     return false
+   else
+     (2..num-1).to_a.all? do |possible_factor|
+       num % possible_factor != 0
+     end
+   end
 
 end
